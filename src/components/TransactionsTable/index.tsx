0a69ded995f21fc174fca740609acff99add19f4
Ayrton Souza <ayrtonsouza@icloud.com>
2021-07-19 23:57:26 -0300

    [:sparkles:] adding api support for application

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
+import { api } from '../../services/api';
 import { Container } from './styles';
 
 function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  }, [])
   return (
     <Container>
       <table>
@@ -21,7 +27,7 @@ function TransactionsTable() {
           </tr>
           <tr>
             <td>Aluguel</td>
-            <td className="withdraw">- R$1.100</td>
+            <td className="withdraw"> - R$1.100</td>
             <td>Casa</td>
             <td>20/02/2021</td>
           </tr>
