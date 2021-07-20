e67eca89059adfe44dc23c9a3da9810460b68c9e
Ayrton Souza <ayrtonsouza@icloud.com>
2021-07-20 16:20:50 -0300

    [:sparkles:] income and outcome buttons structure development

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface Props {
   isOpen: boolean;
@@ -30,6 +32,16 @@ export const NewTransactionModal: React.FC<Props> = ({
       <h2>Cadastrar transação</h2>
       <input placeholder="Título" />
       <input placeholder="Valor" type="number" />
+      <TransactionTypeContainer>
+        <button type="button">
+          <img src={incomeImg} alt="Entrada" />
+          <span>Entrada</span>
+        </button>
+        <button type="button">
+          <img src={outcomeImg} alt="Saída" />
+          <span>Saída</span>
+        </button>
+      </TransactionTypeContainer>
       <input placeholder="Categoria" />
       <button type="submit">Cadastrar</button>
     </Container>
